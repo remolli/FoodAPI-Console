aaa40b9985aff2ee7fab27639b6cd9c14caa4e14
Evandru <evandro_remolli.t@outlook.com>
2023-06-15 15:09:28 -0300

    add: API consume

---
+++ FoodAPI.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodAPI_Console
+{
+    public class FoodAPI
+    {
+        HttpClient httpClient;
+        string apiKey;
+        public FoodAPI(string apiKey)
+        {
+            this.apiKey = apiKey;
+            httpClient = new HttpClient();
+        }
+        public async Task<string> GetJsonString(string link)
+        {
+            var response = await httpClient.GetAsync(link + "?apiKey=" + apiKey);
+            var jsonString = await response.Content.ReadAsStringAsync();
+            return jsonString;
+        }
+    }
+}
