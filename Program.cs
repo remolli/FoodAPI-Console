b8d58c6b2e3f3d471eec7c45865787e67bd0f8f9
Evandru <evandro_remolli.t@outlook.com>
2023-06-15 15:10:38 -0300

    add: FoodAPI instance with apiKey

--- Program.cs
+++ Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using FoodAPI_Console;
+
+const string apiKey = "3a422ff82d024ed489e4d0b9ca4bcb59";
+
+FoodAPI api = new FoodAPI(apiKey);
