0ae7a305c3a4831a754a5f8739f8d086339b6d97
Evandru <evandro_remolli.t@outlook.com>
2023-06-15 16:06:43 -0300

    add: search products HUD

--- Program.cs
+++ Program.cs
@@ -3,3 +3,20 @@
 const string apiKey = "3a422ff82d024ed489e4d0b9ca4bcb59";
 
 FoodAPI api = new FoodAPI(apiKey);
+
+while(true)
+{
+    Console.Write("Escreva o nome de um produto: ");
+    string query = Console.ReadLine();
+
+    var search = await api.GetSearchGroceryProducts(query);
+
+    foreach (var item in search.Products)
+    {
+        Console.WriteLine(item.Id);
+        Console.WriteLine(item.Title);
+        Console.WriteLine(item.ImageType);
+        Console.WriteLine(new string('-', 30));
+    }
+    Console.WriteLine();
+}
