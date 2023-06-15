566d8c4a03fc41b72209ed368bf390eb0cfd7df8
Evandru <evandro_remolli.t@outlook.com>
2023-06-15 16:17:36 -0300

    add: more informations and some colors

--- Program.cs
+++ Program.cs
@@ -6,17 +6,31 @@ FoodAPI api = new FoodAPI(apiKey);
 
 while(true)
 {
+    Console.ForegroundColor = ConsoleColor.Magenta;
     Console.Write("Escreva o nome de um produto: ");
+    Console.ForegroundColor = ConsoleColor.Yellow;
     string query = Console.ReadLine();
 
     var search = await api.GetSearchGroceryProducts(query);
 
     foreach (var item in search.Products)
     {
+        Console.WriteLine();
+        Console.ForegroundColor = ConsoleColor.Green;
         Console.WriteLine(item.Id);
         Console.WriteLine(item.Title);
         Console.WriteLine(item.ImageType);
+        Console.ForegroundColor = ConsoleColor.Gray;
+        Console.WriteLine();
         Console.WriteLine(new string('-', 30));
     }
+
+    Console.ResetColor();
+    Console.WriteLine();
+    Console.WriteLine("Total Products: " + search.TotalProducts);
+    Console.WriteLine("Offset: " + search.Offset);
+    Console.WriteLine("Number: " + search.Number);
+    Console.WriteLine("Type: " + search.Type);
+
     Console.WriteLine();
 }
